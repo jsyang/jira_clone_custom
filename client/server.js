cf32b07d91fd47da85ed06b8301bf1fee0748a37
jsyang <jsyang.ca@gmail.com>
2020-08-14 05:14:29 +0100

    Secure client with basic auth

--- client/server.js
+++ client/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
 const compression = require('compression');
+const basicAuth = require('express-basic-auth');
 
 const app = express();
 
@@ -9,5 +10,14 @@ app.use(compression());
 app.use(express.static(`${__dirname}/build`));
 
 app.use(fallback(`${__dirname}/build/index.html`));
+
+// jsyang: use basic auth to secure everything
+if (process.env.NODE_ENV === 'production') {
+  app.use(
+    basicAuth({
+      users: { skytrak2: 'proposalbeingconsidered' },
+    }),
+  );
+}
 
 app.listen(process.env.PORT || 8081);
